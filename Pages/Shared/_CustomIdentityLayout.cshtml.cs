33208d90301646aa744c346975500a49c0bb21c0
Chinenye Okeke <maryroseokeke@yahoo.com>
2023-05-17 23:30:32 +0100

    Add project files.

---
+++ Pages/Shared/_CustomIdentityLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityTodo.Pages.Shared
+{
+    public class _CustomIdentityLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Services/ConsoleEmailSender.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Web;
+
+namespace IdentityTodo.Services
+{
+    public class ConsoleEmailSender : IEmailSender
+    {
+
+        public Task SendEmailAsync(string emailAddress, string subject, string htmlMessage)
+        {
+            Console.WriteLine("---New Email----");
+            Console.WriteLine($"To: {emailAddress}");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine(HttpUtility.HtmlDecode(htmlMessage));
+            Console.WriteLine("-------");
+
+            return Task.CompletedTask;
+        }
+    }
+}
